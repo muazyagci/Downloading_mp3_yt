3382076c058de8580145e0c49a476f060701266a
Muaz Yagci <m.muazyagci@gmail.com>
2023-09-28 23:49:55 +0200

    Success of downloading mp3 from yt

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        List<String> urlCollection = new ArrayList<>();
+
+        // Add the URLs to the collection
+        urlCollection.add("https://youtu.be/tEH1cF8HDGY?si=OKTzpjgNrrgiujgz");
+        urlCollection.add("https://youtu.be/Pl7--yxZjng?si=XBYnKBXvw1_z2Q-9");
+        urlCollection.add("https://youtu.be/XHwOPkFMtZg?si=sAHGQXwmv3pV9YOJ");
+        urlCollection.add("https://youtu.be/P8K0d6mw71Q?si=5rQkf2p7aummZupn");
+        urlCollection.add("https://youtu.be/xpjRba5c7-s?si=kp6-dkj6IzSalmcA");
+        urlCollection.add("https://youtu.be/rE7O2niluqE?si=sZsJx6-n4DL3Zf7p");
+
+        // You can now iterate over the collection using a forEach loop or any other method.
+        for (String url : urlCollection) {
+            driver.get("https://ytmp3.nu/nBlF/");
+            driver.findElement(By.id("url")).sendKeys(url);
+            driver.findElement(By.cssSelector("input[type='submit']")).click();
+           Thread.sleep(1500);
+            driver.findElement(By.xpath("(//a[@rel='nofollow'])[2]")).click();
+
+        }
+
+    }
+}
